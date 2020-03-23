b69fe0745729a8de394e97f3a67e0ea86a370887
Martina <martina.ria@hotmail.com>
2020-03-23 21:03:30 +0100

    Prova validazione email
    if ((emailUtente[a] == "@") && ((emailUtente[a] == ".it") || (mailUtente[a] == ".it") ))

---
+++ Ex1/richieste base/master2.js
@@ -0,0 +1,17 @@
+
+//lista utenti ammessi (array)
+var lista = ["utente1@gmail.com", "utente2@hotmail.com", "utente3@miodominio.it"];
+console.log(lista)
+
+//chiedi email
+var emailUtente = prompt("Prego inserire indirizzo mail");
+
+for (var a = 0; a < emailUtente.length; a++) {
+  if ((emailUtente[a] == "@") && ((emailUtente[a] == ".it") || (mailUtente[a] == ".it") )) {
+    console.log("ok")
+  }
+}
+
+alert("L' indirizzo insertito non è valido");
+
+var emailUtente = prompt("Prego inserire indirizzo mail");
